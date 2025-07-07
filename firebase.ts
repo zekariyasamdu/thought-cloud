611df949aa9be025c495b77358a79fdce4086067
missinggoverner <zekariyasamdu24@gmail.com>
2025-07-07 17:27:03 +0300

    feat: signup

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -15,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
 
-export { auth , db };
+export { auth , db, googleProvider };
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,47 +1,94 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import {
     Card,
     CardContent,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
+import { auth, googleProvider } from '../../../../firebase'
+import { useRouter } from 'next/navigation'
+
+
 function SignUpPage() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [reEnterPassword, setReEnterPassword] = useState("")
+    const router = useRouter()
+
+    async function handelSubmition(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        console.log("here")
+        if (password === reEnterPassword) {
+            try {
+                await createUserWithEmailAndPassword(
+                    auth,
+                    email,
+                    password
+                );
+                router.push('/auth/login')
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    }
+    async function signInWithGoogle(){
+        try {
+                await signInWithPopup(
+                    auth,
+                    googleProvider
+                );
+                router.push('/auth/login')
+            } catch (e) {
+                console.error(e);
+            }
+    }
+
     return (
         <Card className="w-7xl max-w-sm">
             <CardHeader>
                 <CardTitle> Sign Up</CardTitle>
             </CardHeader>
             <CardContent>
-                <form>
-                    <div className="flex flex-col gap-6">
-                        <div className="grid gap-2">
-                            <Input
-                                id="email"
-                                type="email"
-                                placeholder="email"
-                                required
-                            />
-                        </div>
-                        <div className="grid gap-2">
-                            <Input id="password" type="password" placeholder='password' required />
-                        </div>
-                        <div className="grid gap-2">
-                            <Input id="re-password" type="password" placeholder='re-enter password' required />
-                        </div>
+                <form className="flex flex-col gap-6" onSubmit={(e) => { handelSubmition(e) }}>
+                    <div className="grid gap-2">
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="email"
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            required
+                        />
+                    </div>
+                    <div className="grid gap-2">
+                        <Input
+                            id="password"
+                            type="password"
+                            placeholder='password'
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            required />
+                    </div>
+                    <div className="grid gap-2">
+                        <Input
+                            id="re-password"
+                            type="password"
+                            placeholder='re-enter password'
+                            onChange={(e) => { setReEnterPassword(e.target.value) }}
+                            required />
+                    </div>
+                    <div className="flex-col gap-2">
+                        <Button type="submit" className="w-full cursor-pointer" >
+                            Sign Up
+                        </Button>
+                        <Button variant="outline" className="w-full cursor-pointer" onClick={signInWithGoogle}>
+                            Sign Up with Google
+                        </Button>
                     </div>
                 </form>
             </CardContent>
-            <CardFooter className="flex-col gap-2">
-                <Button type="submit" className="w-full cursor-pointer">
-                    Sign Up
-                </Button>
-                <Button variant="outline" className="w-full cursor-pointer">
-                    Sign Up with Google
-                </Button>
-            </CardFooter>
         </Card>
     )
 }
