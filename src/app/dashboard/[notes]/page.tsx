3d260579c7c90dec8deeab9351f053406f3a4d1f
zekariyasamdu <zekariyasamdu24@gmail.com>
2026-04-28 20:09:05 +0300

    init

--- src/app/dashboard/[notes]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Note() {
-    return (
-        <div>note</div>
-    )
-}
-
-export default Note
--- src/types/notes.ts
+++
@@ -1,7 +0,0 @@
-export interface INote {
-    id: string,
-    title: string, 
-    time: string,
-    content: string
-    public: boolean
-}
